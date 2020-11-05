2097ed9a4f6e9a5fef2b90b43c30734f24f7be47
Prasad <Manish.Prasad2@ingrammicro.com>
2020-11-05 17:14:58 +0530

    Add project files.

---
+++ ConsoleAppDemo01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleAppDemo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            someMethod("Good");
+
+        }
+
+        private static void someMethod(string val)
+        {
+            bool blnWork = true;
+            //if (val == "Good")
+            //    blnWork = true;
+            //else
+            //    blnWork = false;
+            if (blnWork)
+                Console.WriteLine("Good Work");
+            else
+                Console.WriteLine("Needs Inprovement");
+
+            Console.ReadLine();
+        }
+    }
+}
